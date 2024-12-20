a6453d7687b943d922bd9d26842c1c97d61ffa23
Leon Berenschot <leon@wendbaar.nl>
2024-12-20 15:15:55 +0100

    tmp debugging

--- app/parser.rb
+++ app/parser.rb
@@ -14,18 +14,28 @@ class Parser
   def call
     urls.map do |url|
       next unless url =~ /\/events\//
+      # pp url
       Nokogiri::HTML(URI.open(url, **uri_open_headers)).css(".detail_content").map do |node|
+        # pp node.to_html
+        # pp
+        # detail_content(node)
         Hash[detail_content(node)]
       end
+      # break
     end.flatten
   end
 
   def detail_content(node)
+    # pp node.to_html
+    # puts "detail_content"
     node.css('.detail_item').map do |wrapper|
+      # puts "  detail_item"
       wrapper.css('.detail_box').map do |box|
+        # puts "    detail_box"
         box.css('> div').map do |text|
           val = text.content.gsub(/[[:space:]]+/, " ").strip
           next if val =~ /\d+ ?\/ ?\d+ ?\/ ?\d+/
+          # puts "     div: #{val}"
           val
         end.compact
       end.transpose
