2dc23971c57699e4219f40f6100249a6a5a149e6
Leon Berenschot <leon@wendbaar.nl>
2024-12-20 21:27:58 +0100

    spec: use a copy of a real feed

--- spec/calendar_builder_spec.rb
+++ spec/calendar_builder_spec.rb
@@ -18,10 +18,17 @@ RSpec.describe CalendarBuilder do
       # ticket_link: "https://burgerweeshuis.stager.co/web/tickets/111436027",
     }
   }
+  let(:json) { JSON.parse(File.read('src/events.json'), symbolize_names: true) }
 
   it "creates an ical feed" do
     expect {
       described_class.new([event]).call
+    }.not_to raise_error
+  end
+
+  it "creates an ical feed" do
+    expect {
+      described_class.new(json).call
     }.not_to raise_error
   end
 end
