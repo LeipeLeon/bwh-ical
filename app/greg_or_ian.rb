7752361c6bcd03bf798132014b1e5f6befe934ba
Leon Berenschot <leon@wendbaar.nl>
2025-01-23 20:56:59 +0100

    🐛 Use %u, not %w
    because the weekend rollover:
    
    1..7 % 2 = 1,0,1,0,1,0,1 # -> double 1
    0..6 % 2 = 0,1,0,1,0,1,0 # -> double 0
    
    Weekday:
      %u - Day of the week (Monday is 1, 1..7)
      %w - Day of the week (Sunday is 0, 0..6)
    
    ```ruby
    Timecop.freeze(Time.local(2025, 1, 25))
    (Date.today).step(Date.today + 31).map do |date|
      wday = date.strftime("%u").to_i % 2
      pp [date.strftime("%d %m %u"), wday, GREG_OR_IAN[wday]]
    end
    ```

--- app/greg_or_ian.rb
+++ app/greg_or_ian.rb
@@ -11,13 +11,13 @@ class GregOrIan
     cal.append_custom_property "X_WR_CALDESC:", "Monday: Greg, Tuesday: Ian, Wednesday: Greg, Thursday: Ian, Friday: Greg, Saturday: Ian, Sunday: Greg. _The Gregorian calendar_"
     cal.append_custom_property "X_ORIGINAL_URL", "https://leipeleon.github.io/bwh-ical/"
     (Date.today - width).step(Date.today + width).map do |date|
-      its_greg_or_ian = GREG_OR_IAN[date.strftime("%w").to_i % 2]
+      its_greg_or_ian = GREG_OR_IAN[date.strftime("%u").to_i % 2]
       event             = Icalendar::Event.new
       event.dtstart     = Icalendar::Values::Date.new(date)
       event.summary     = "It's #{its_greg_or_ian} day"
       event.description = "See: https://mikecoats.com/the-greg-or-ian-calendar/"
       event.url         = "https://mikecoats.com/the-greg-or-ian-calendar/"
-      event.uid         = "LeipeLeon-greg-or-ian-generator-%s" % date.strftime("%d-%m-%Y")
+      event.uid         = "LeipeLeon-greg-or-ian-generator-%s-%s" % [date.strftime("%a-%d-%m-%Y"), its_greg_or_ian]
 
       cal.add_event(event)
     end
--- spec/greg_or_ian_spec.rb
+++ spec/greg_or_ian_spec.rb
@@ -5,11 +5,39 @@ require_relative "../app/greg_or_ian.rb"
 
 RSpec.describe GregOrIan do
 
-  before { Timecop.freeze(Time.local(2025, 1, 23)) }
+  before { Timecop.freeze(Time.local(2025, 1, 25)) }
   after { Timecop.return }
 
+  it "has the proper values" do
+    expected = [
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Greg day",
+      "SUMMARY:It's Ian day"
+    ]
+    inspected = subject.call(10).split("\r\n").select { _1 =~ /SUMMARY/}
+    expect(inspected).to eql(expected)
+  end
+
   it "creates an ical feed" do
-    expect(subject.call(1)).to eql <<~EVENT
+    expect(subject.call(2)).to eql <<~EVENT
       BEGIN:VCALENDAR\r
       VERSION:2.0\r
       PRODID:-//LeipeLeon//greg-or-ian-generator/\r
@@ -20,27 +48,43 @@ RSpec.describe GregOrIan do
        \\, Friday: Greg\\, Saturday: Ian\\, Sunday: Greg. _The Gregorian calendar_\r
       X-ORIGINAL-URL:https://leipeleon.github.io/bwh-ical/\r
       BEGIN:VEVENT\r
-      DTSTAMP:20250123T000000Z\r
-      UID:LeipeLeon-greg-or-ian-generator-22-01-2025\r
-      DTSTART;VALUE=DATE:20250122\r
+      DTSTAMP:20250125T000000Z\r
+      UID:LeipeLeon-greg-or-ian-generator-Thu-23-01-2025-Ian\r
+      DTSTART;VALUE=DATE:20250123\r
       DESCRIPTION:See: https://mikecoats.com/the-greg-or-ian-calendar/\r
       SUMMARY:It's Ian day\r
       URL;VALUE=URI:https://mikecoats.com/the-greg-or-ian-calendar/\r
       END:VEVENT\r
       BEGIN:VEVENT\r
-      DTSTAMP:20250123T000000Z\r
-      UID:LeipeLeon-greg-or-ian-generator-23-01-2025\r
-      DTSTART;VALUE=DATE:20250123\r
+      DTSTAMP:20250125T000000Z\r
+      UID:LeipeLeon-greg-or-ian-generator-Fri-24-01-2025-Greg\r
+      DTSTART;VALUE=DATE:20250124\r
       DESCRIPTION:See: https://mikecoats.com/the-greg-or-ian-calendar/\r
       SUMMARY:It's Greg day\r
       URL;VALUE=URI:https://mikecoats.com/the-greg-or-ian-calendar/\r
       END:VEVENT\r
       BEGIN:VEVENT\r
-      DTSTAMP:20250123T000000Z\r
-      UID:LeipeLeon-greg-or-ian-generator-24-01-2025\r
-      DTSTART;VALUE=DATE:20250124\r
+      DTSTAMP:20250125T000000Z\r
+      UID:LeipeLeon-greg-or-ian-generator-Sat-25-01-2025-Ian\r
+      DTSTART;VALUE=DATE:20250125\r
       DESCRIPTION:See: https://mikecoats.com/the-greg-or-ian-calendar/\r
       SUMMARY:It's Ian day\r
+      URL;VALUE=URI:https://mikecoats.com/the-greg-or-ian-calendar/\r
+      END:VEVENT\r
+      BEGIN:VEVENT\r
+      DTSTAMP:20250125T000000Z\r
+      UID:LeipeLeon-greg-or-ian-generator-Sun-26-01-2025-Greg\r
+      DTSTART;VALUE=DATE:20250126\r
+      DESCRIPTION:See: https://mikecoats.com/the-greg-or-ian-calendar/\r
+      SUMMARY:It's Greg day\r
+      URL;VALUE=URI:https://mikecoats.com/the-greg-or-ian-calendar/\r
+      END:VEVENT\r
+      BEGIN:VEVENT\r
+      DTSTAMP:20250125T000000Z\r
+      UID:LeipeLeon-greg-or-ian-generator-Mon-27-01-2025-Greg\r
+      DTSTART;VALUE=DATE:20250127\r
+      DESCRIPTION:See: https://mikecoats.com/the-greg-or-ian-calendar/\r
+      SUMMARY:It's Greg day\r
       URL;VALUE=URI:https://mikecoats.com/the-greg-or-ian-calendar/\r
       END:VEVENT\r
       END:VCALENDAR\r
